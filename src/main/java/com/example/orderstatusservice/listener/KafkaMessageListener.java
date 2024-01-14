b141b03eb2cf2f8e16ca794dd50a0fb452ba6765
Abriel <abrielz11@gmail.com>
2024-01-14 19:30:11 +0300

    i add sender listener

--- src/main/java/com/example/orderstatusservice/listener/KafkaMessageListener.java
+++ src/main/java/com/example/orderstatusservice/listener/KafkaMessageListener.java
@@ -4,13 +4,21 @@ import com.example.orderstatusservice.model.KafkaMessage;
 import com.example.orderstatusservice.service.KafkaMessageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import java.util.Properties;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.retrytopic.DestinationTopic;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 @Slf4j
 @Component
@@ -21,7 +29,7 @@ public class KafkaMessageListener {
 
     private final KafkaTemplate<String, KafkaMessage> kafkaTemplate;
 
-    @KafkaListener(topics = "${app.kafka.kafkaMessageTopic}",
+    @KafkaListener(topics = "${app.kafka.topicToRead}",
                    groupId = "${app.kafka.kafkaMessageGroupId}",
                    containerFactory = "kafkaMessageConcurrentKafkaListenerContainerFactory")
     public void listen(@Payload KafkaMessage message,
@@ -34,7 +42,28 @@ public class KafkaMessageListener {
         log.info("Key: {}; Partition: {}; Topic: {}; Timestamp: {}", key, partition, topic, timeStamp);
 
         kafkaMessageService.add(message);
-        kafkaMessageService.doSomethingWithMessage(message);
+     //   kafkaMessageService.doSomethingWithMessage(message);
+    //    doSomethingWithMessage(topic, message);
+    }
+
+//    public void doSomethingWithMessage(String topicName, KafkaMessage message){
+//
+//       // topicName = "topic_two";
+//        kafkaTemplate.send(topicName, message);
+//    }
+
+    @KafkaListener(topics = "${app.kafka.topicToWrite}",
+            groupId = "${app.kafka.kafkaMessageGroupId}",
+            containerFactory = "kafkaMessageConcurrentKafkaListenerContainerFactory")
+    public void send(@Payload KafkaMessage message,
+                     @Header(value = KafkaHeaders.RECEIVED_KEY, required = false) UUID key,
+                     @Header(value = KafkaHeaders.RECEIVED_TOPIC) String topic,
+                     @Header(value = KafkaHeaders.RECEIVED_PARTITION) Integer partition,
+                     @Header(value = KafkaHeaders.RECEIVED_TIMESTAMP) Long timeStamp) {
+
+        log.info("Received message: {}", message);
+        log.info("Key: {}; Partition: {}; Topic: {}; Timestamp: {}", key, partition, topic, timeStamp);
+
         doSomethingWithMessage(topic, message);
     }
 
--- src/main/java/com/example/orderstatusservice/service/KafkaMessageService.java
+++ src/main/java/com/example/orderstatusservice/service/KafkaMessageService.java
@@ -1,12 +1,24 @@
 package com.example.orderstatusservice.service;
 
 import com.example.orderstatusservice.model.KafkaMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class KafkaMessageService {
+
+   // private final KafkaTemplate<String, KafkaMessage> kafkaTemplate;
 
     private final List<KafkaMessage> messages = new ArrayList<>();
 
@@ -14,10 +26,23 @@ public class KafkaMessageService {
         messages.add(message);
     }
 
-    public void doSomethingWithMessage(KafkaMessage message) {
-        //todo сделать тело
-    }
-
-    public void send(KafkaMessage someMessageToAnotherTopic) {
-    }
+//    @KafkaListener(topics = "${app.kafka.topicToWrite}",
+//            groupId = "${app.kafka.kafkaMessageGroupId}",
+//            containerFactory = "kafkaMessageConcurrentKafkaListenerContainerFactory")
+//    public void send(@Payload KafkaMessage message,
+//                     @Header(value = KafkaHeaders.RECEIVED_KEY, required = false) UUID key,
+//                     @Header(value = KafkaHeaders.RECEIVED_TOPIC) String topic,
+//                     @Header(value = KafkaHeaders.RECEIVED_PARTITION) Integer partition,
+//                     @Header(value = KafkaHeaders.RECEIVED_TIMESTAMP) Long timeStamp) {
+//
+//        log.info("Received message: {}", message);
+//        log.info("Key: {}; Partition: {}; Topic: {}; Timestamp: {}", key, partition, topic, timeStamp);
+//
+//            doSomethingWithMessage(topic, message);
+//    }
+//
+//    public void doSomethingWithMessage(String topicName, KafkaMessage message){
+//
+//        kafkaTemplate.send(topicName, message);
+//    }
 }
